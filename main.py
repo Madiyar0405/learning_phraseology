61d0b575cd615ee7a6aa9960d8735ffeab24c793
GeekPerson <123325330+Madiyar0405@users.noreply.github.com>
2024-02-27 15:14:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import streamlit as st
 from phraseology.phraseology_app import main as phraseology_main
-from quiz.quiz_app import main as quiz_main
+from quiz.quiz_app import run_quiz  # Corrected import statement
 from mainPage.info import main as main_page
-
 
 st.set_page_config(layout="wide")
 
-
 page = st.sidebar.selectbox("Выберите страницу", ["Главная страница", "Фразеологизмы", "Викторина"])
 
-
-if  page  == "Главная страница":
+if page == "Главная страница":
     main_page()
 elif page == "Фразеологизмы":
     phraseology_main()
 elif page == "Викторина":
-    quiz_main()
+    run_quiz()  # Changed to call the run_quiz function
